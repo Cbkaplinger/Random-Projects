ee068f3ea847fb4eb3795e8dbc8c3e547d2b175c
Cbkaplinger <cameron.kaplinger@gmail.com>
2024-01-08 20:11:24 -0500

    Attempted to mess with CBB and started messing with batter data

---
+++ CBB/CBBdata.R
@@ -0,0 +1,7 @@
+
+library(cbbdata)
+
+cbd_create_account("cbkaplinger", "cameron.kaplinger@gmail.com", "Camcam1215", "Camcam1215")
+
+cbd_login(username = Sys.getenv("cbkaplinger"), password = Sys.getenv("Camcam1215"))
+
